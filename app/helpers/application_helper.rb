13b9870c833184efb1df02a65f8f0b94d31d7921
websoft foryou <xxD8j3SWCAZPrfEaxT84>
2023-04-02 21:06:27 -0400

    add helper for api request

---
+++ app/helpers/application_helper.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+
+module ApplicationHelper
+  require 'uri'
+  require 'net/http'
+
+  def get_request(request_url)
+    url = URI.parse(ENV['APP_HOST'] + ':' + ENV['APP_PORT'] + request_url)
+    request = Net::HTTP::Get.new(url)
+    response = Net::HTTP.start(url.hostname, url.port) { |http|
+      http.request(request)
+    }
+    JSON.parse(response.body)
+  end
+
+  def post_request(request_url, data)
+    url = URI.parse(ENV['APP_HOST'] + ':' + ENV['APP_PORT'] + request_url)
+    request = Net::HTTP::Post.new(url)
+    request.set_form_data(data)
+    response = Net::HTTP.start(url.hostname, url.port) do |http|
+      http.request(request)
+    end
+
+    JSON.parse(response.body)
+  end
+
+  def delete_request(request_url)
+    url = URI.parse(ENV['APP_HOST'] + ':' + ENV['APP_PORT'] + request_url)
+    request = Net::HTTP::Delete.new(url)
+    response = Net::HTTP.start(url.hostname, url.port) do |http|
+      http.request(request)
+    end
+
+    JSON.parse(response.body)
+  end
+
+  def update_request(request_url, data)
+    url = URI.parse(ENV['APP_HOST'] + ':' + ENV['APP_PORT'] + request_url)
+    request = Net::HTTP::Put.new(url)
+    request.set_form_data(data)
+    response = Net::HTTP.start(url.hostname, url.port) do |http|
+      http.request(request)
+    end
+
+    JSON.parse(response.body)
+  end
+end
