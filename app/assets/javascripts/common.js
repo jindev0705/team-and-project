13910af395da1213cbf77e2efcf7767288d38816
websoft foryou <xxD8j3SWCAZPrfEaxT84>
2023-04-02 21:01:30 -0400

    add assets(javascript, css) for ui

---
+++ app/assets/javascripts/common.js
@@ -0,0 +1,21 @@
+function showMessage(message_type, message) {
+    toastr.options = {
+        closeButton: false,
+        debug: false,
+        newestOnTop: false,
+        progressBar: false,
+        positionClass: "toast-top-right",
+        preventDuplicates: false,
+        onclick: null,
+        showDuration: "300",
+        hideDuration: "1000",
+        timeOut: "5000",
+        extendedTimeOut: "1000",
+        showEasing: "swing",
+        hideEasing: "linear",
+        showMethod: "fadeIn",
+        hideMethod: "fadeOut",
+    };
+    if (message_type == 'success') toastr.success(message);
+    if (message_type == 'error') toastr.error(message);
+}
