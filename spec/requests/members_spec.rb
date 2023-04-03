cbc6f1f0a86ca571743854c5889bb304338dcd28
websoft foryou <xxD8j3SWCAZPrfEaxT84>
2023-04-02 21:49:34 -0400

    add test code for member post api

---
+++ spec/requests/members_spec.rb
@@ -0,0 +1,47 @@
+require 'rails_helper'
+
+RSpec.describe Api::MembersController, type: :request do
+
+  describe "POST /api/members" do
+    let(:team) { Team.create(name: 'my team') }
+    context 'with valid parameters' do
+      let(:valid_params) { { first_name: 'rails', last_name: 'developer', city: 'rails developer city', state: 'rails developer state', country: 'rails developer country', team_id: team.id }}
+      it 'creates a new post' do
+        expect {
+          post api_members_url, params: valid_params
+        }.to change(Member, :count).by(1)
+      end
+
+      it 'returns a status code of 201' do
+        post api_members_url, params: valid_params
+        expect(response).to have_http_status(200)
+      end
+
+      it 'returns the newly created member as JSON' do
+        post api_members_url, params: valid_params
+        expect(JSON.parse(response.body)['member'].to_json).to eq(Member.last.to_json)
+      end
+    end
+
+    context 'with invalid parameters' do
+      let(:invalid_params) { { first_name: '', last_name: '', team_id: '' } }
+
+      it 'does not create a new member' do
+        expect {
+          post api_members_url, params: invalid_params
+        }.not_to change(Member, :count)
+      end
+
+      it 'returns a status code of 422' do
+        post api_members_url, params: invalid_params
+        expect(response).to have_http_status(422)
+      end
+
+      it 'returns an error message as JSON' do
+        post api_members_url, params: invalid_params
+        expect(response.body).to eq({ error: { first_name: ["can't be blank"], last_name: ["can't be blank"], team_id: ["can't be blank"], team: ["must exist"] } }.to_json)
+      end
+    end
+
+  end
+end
